b70de77555d16e76387cc3f4df238e66cba9c357
Abdeljabar Ferdjani <abdeljabarferdjani@MacBook-Air-de-Abdeljabar.local>
2018-10-10 17:48:49 +0200

    style bandeau

--- app/src/main/java/com/example/kruse/myapplication/Chat.java
+++ app/src/main/java/com/example/kruse/myapplication/Chat.java
@@ -1,10 +1,15 @@
 package com.example.kruse.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +20,14 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        Button buttonProfile = findViewById(R.id.profile);
+
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonProfileClick();
+            }
+        });
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_id);
 
@@ -38,4 +51,11 @@ public class Chat extends AppCompatActivity {
         ItemAdapter adapter = new ItemAdapter(messages, this);
         recyclerView.setAdapter(adapter);
     }
+
+    private void onButtonProfileClick() {
+
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
 }
