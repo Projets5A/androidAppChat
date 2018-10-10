8bf9184454130668e99725339ceadce2614fc692
Abdeljabar Ferdjani <abdeljabarferdjani@MacBook-Air-de-Abdeljabar.local>
2018-10-10 14:44:53 +0200

    Recycler view start

--- app/src/main/java/com/example/kruse/myapplication/Chat.java
+++ app/src/main/java/com/example/kruse/myapplication/Chat.java
@@ -1,11 +1,13 @@
 package com.example.kruse.myapplication;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Chat extends AppCompatActivity {
 
@@ -13,18 +15,27 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_id);
+
+        GetGroupeMessagesTask getGroupMessages = new GetGroupeMessagesTask();
+        getGroupMessages.execute();
+
+        List<String> messages = new ArrayList<>();
+
+        messages.add("Hello world");
+        messages.add("Hello world");
+        messages.add("Hello world");
+        messages.add("Hello world");
+        messages.add("Hello world");
+        messages.add("Hello world");
+
+        // use a linear layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter
+        ItemAdapter adapter = new ItemAdapter(messages, this);
+        recyclerView.setAdapter(adapter);
     }
-
 }
--- app/src/main/java/com/example/kruse/myapplication/GetGroupeMessagesTask.java
+++ app/src/main/java/com/example/kruse/myapplication/GetGroupeMessagesTask.java
@@ -1,30 +1,21 @@
 package com.example.kruse.myapplication;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-    private final String mPseudo;
-    private final String mPassword;
-    private final OnLoginChangeListener listener;
-
-    UserLoginTask(String pseudo, String password, OnLoginChangeListener listener) {
-        mPseudo = pseudo;
-        mPassword = password;
-        this.listener = listener;
-    }
-
-    @Override
+public class GetGroupeMessagesTask extends AsyncTask<Void, Void, Boolean> {
     protected Boolean doInBackground(Void... params) {
         int responseCode = 401;
         try {
-            String myUrl = "https://training.loicortola.com/chat-rest/1.0/connect/" + mPseudo + "/" + mPassword;
+            String myUrl = "http://localhost:3000/getGroupeMessages";
             URL url = new URL(myUrl);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             responseCode = urlConnection.getResponseCode();
+            Log.i("tag", "" + responseCode);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,6 +26,6 @@ public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
     @Override
     protected void onPostExecute(final Boolean success) {
-        listener.signupChange(success);
+        Log.i("tag", "getmessagesgroup success!");
     }
 }
